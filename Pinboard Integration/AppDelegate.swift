c745cf10634060f0799e4fb3ad4e454b645aba75
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2018-12-30 14:27:56 +0100

    Initial Commit

---
+++ Pinboard Integration/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Pinboard Integration
+//
+//  Created by Kristof Adriaenssens on 30/12/2018.
+//  Copyright © 2018 Kristof Adriaenssens. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
---
+++ Pinboard IntegrationTests/Pinboard_IntegrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pinboard_IntegrationTests.swift
+//  Pinboard IntegrationTests
+//
+//  Created by Kristof Adriaenssens on 30/12/2018.
+//  Copyright © 2018 Kristof Adriaenssens. All rights reserved.
+//
+
+import XCTest
+@testable import Pinboard_Integration
+
+class Pinboard_IntegrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
