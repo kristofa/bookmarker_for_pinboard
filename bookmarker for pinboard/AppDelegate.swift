a50c339abeb557b2f88e3793da08d1f042a470e8
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2019-02-19 21:06:33 +0100

    Automatically close application when main window is closed

--- bookmarker for pinboard/AppDelegate.swift
+++ bookmarker for pinboard/AppDelegate.swift
@@ -11,4 +11,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
 }
