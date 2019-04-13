b9594923989356ea72f389e54cd095ab470988e8
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2019-04-13 17:16:37 +0200

    Save bookmark when enter is pressed in text fields
    Fixes #10
    
    This change saves the bookmark when you press enter in the description
    or tags text fields.

--- bookmarker for pinboard extension/SafariExtensionViewController.swift
+++ bookmarker for pinboard extension/SafariExtensionViewController.swift
@@ -20,44 +20,55 @@ class SafariExtensionViewController: SFSafariExtensionViewController {
     @IBOutlet weak var readLaterCheckbox: NSButton!
     @IBOutlet weak var privateCheckbox: NSButton!
     
+    @IBAction func tagsTextFieldAction(_ sender: Any) {
+        saveBookMark()
+    }
+    
+    @IBAction func descriptionTextFieldAction(_ sender: Any) {
+        saveBookMark()
+    }
+    
     @IBAction func bookmarkButtonPressed(_ sender: Any) {
-        
+        saveBookMark()
+    }
+    
+    private func saveBookMark() -> Void {
         var apiTokenFromKeychain: String
         let response = apiTokenAccess.getApiToken()
         
         switch(response) {
-            case .ErrorApiTokenItemNotFound:
-                let message = "Can't find API token in keychain. Set it using application."
-                self.updateStatusTextFieldFailure(message: message)
-                return
-            case .ErrorUnexpectedApiTokenData:
-                let message = "Invalid data for API key in keychain. Manually remove it from keychain."
-                self.updateStatusTextFieldFailure(message: message)
-                return
-            case .ErrorUnknown(let status):
-                let message = "Unknown error when getting API key from keychain. OSStatus: \(status)"
-                self.updateStatusTextFieldFailure(message: message )
-                return
-            case .Success(let token):
-                NSLog("Getting API token from keychain successful")
-                apiTokenFromKeychain = token
+        case .ErrorApiTokenItemNotFound:
+            let message = "Can't find API token in keychain. Set it using application."
+            self.updateStatusTextFieldFailure(message: message)
+            return
+        case .ErrorUnexpectedApiTokenData:
+            let message = "Invalid data for API key in keychain. Manually remove it from keychain."
+            self.updateStatusTextFieldFailure(message: message)
+            return
+        case .ErrorUnknown(let status):
+            let message = "Unknown error when getting API key from keychain. OSStatus: \(status)"
+            self.updateStatusTextFieldFailure(message: message )
+            return
+        case .Success(let token):
+            NSLog("Getting API token from keychain successful")
+            apiTokenFromKeychain = token
         }
         
         let pinboardUrl = PinboardUrl(url: urlTextField.stringValue, title: titleTextField.stringValue, description: descriptionTextField.stringValue, isPrivate: buttonStateToBool(value: privateCheckbox.state), readLater: buttonStateToBool(value: readLaterCheckbox.state), tags: tagsTextField.stringValue)
         
         pinboardApi.submit(apiToken: apiTokenFromKeychain, pinboardUrl: pinboardUrl) {
             (url, response) in
-                switch response {
-                    case .Succes:
-                        NSLog("Pinboard Request successful")
-                        self.updateStatusTextFieldSuccess(message: "URL successfully submitted to Pinboard.")
-                        self.dismiss(self)
-                    case .Error(let value):
-                        NSLog("Pinboard Request failed. Error: \(value)")
-                        self.updateStatusTextFieldFailure(message: value)
-                }
+            switch response {
+            case .Succes:
+                NSLog("Pinboard Request successful")
+                self.updateStatusTextFieldSuccess(message: "URL successfully submitted to Pinboard.")
+                self.dismiss(self)
+            case .Error(let value):
+                NSLog("Pinboard Request failed. Error: \(value)")
+                self.updateStatusTextFieldFailure(message: value)
+            }
         }
-        
+
     }
     
     private func buttonStateToBool(value: NSControl.StateValue) -> Bool {
