0fd8edbcbb37d9133f34c0991a1cf533b417e47a
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2019-05-15 16:33:32 +0200

    Comment out debug log message

--- bookmarker for pinboard extension/SafariExtensionHandler.swift
+++ bookmarker for pinboard extension/SafariExtensionHandler.swift
@@ -8,7 +8,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
         page.getPropertiesWithCompletionHandler { properties in
             
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+            //NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
             
             if let selectedText = userInfo?["text"] {
                 DispatchQueue.main.async {
