359ce558ed5c6018c5751fcd79e5f14022241489
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2019-05-15 16:34:06 +0200

    Change way to get selected text in injected script
    We use a 'selectionchange' event listener and store the selected text, on every change, in a variable
    instead of getting the selected text on demand when opening the popover window.
    We do this to work around the deselect issue when the toolbar button is pressed.
    
    We store the selected text in the 'selectedText' variable but only empty it if we got
    two or more consecutive empty selections. This to work around the deselect issue when the toolbar button is pressed.
    Following sequences work should work:
    
    a. 1. User selects text.  2. selection is lost when user presses toolbar button (outside of our control)
    b. 1. User deselects text. 2. selection is lost when user presses toolbar button (outside of our control)
    
    In a. the user selected text will appear in the Description field as expected as we only got a single empty selection (2.)
    In b. the Description field will be empty as expected because we got 2 consecutive empty selections (1. and 2.)

--- bookmarker for pinboard extension/script.js
+++ bookmarker for pinboard extension/script.js
@@ -2,15 +2,42 @@ if (window.top === window) {
     // The parent frame is the top-level frame, not an iframe.
     // All non-iframe code goes before the closing brace.
     
+    var selectedText = ""
+    var numberConsecutiveEmptySelections = 0
+    
     var handleMessage = function (event) {
-        console.log("(bookmarker for pinboard) Get message from extension %s.",event.name);
+        //console.log("(bookmarker for pinboard) Got message from extension %s.", event.name);
         switch(event.name) {
             case "getSelectedText" :
-                safari.extension.dispatchMessage("selectedText", {"text": window.getSelection().toString()} );
+                safari.extension.dispatchMessage("selectedText", {"text": selectedText} );
         }
     }
     
     document.addEventListener("DOMContentLoaded", function(event) {
-                              safari.self.addEventListener("message", handleMessage);
+                                safari.self.addEventListener("message", handleMessage);
+                              });
+    
+    document.addEventListener("selectionchange", () => {
+                                // This implementation with 'numberConsecutiveEmptySelections' is implemented
+                                // to work around and ignore the deselect of text when we press the toolbar button.
+                                // We store the selected text in the 'selectedText' variable but only empty it if we got
+                                // two or more consecutive empty selections. This means following sequences work :
+                                // a. 1. User selects text.  2. selection is lost when user presses toolbar button (outside of our control)
+                                // b. 1. User deselects text. 2. selection is lost when user presses toolbar button (outside of our control)
+                              
+                                // In a. the user selected text will appear in the Description field as expected.
+                                // In b. the Description field will be empty as expected.
+                              
+                                newSelection = window.getSelection().toString()
+                                if (newSelection == "") {
+                                    numberConsecutiveEmptySelections++
+                                    if (numberConsecutiveEmptySelections >= 2) {
+                                        selectedText = ""
+                                    }
+                                } else {
+                                    selectedText = newSelection
+                                    numberConsecutiveEmptySelections = 0
+                                }
+                                //console.log(selectedText);
                               });
 }
